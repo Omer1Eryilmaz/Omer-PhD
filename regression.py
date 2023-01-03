c4c198fc723ca78b200b0b89035e113bdac803cb
Omer Eryilmaz <omerb.eryilmaz@gmail.com>
2023-01-03 18:02:49 +0000

    son hali

--- regression.py
+++ regression.py
@@ -15,15 +15,20 @@ from torch.utils.data.dataloader import DataLoader
 #4- Print loss
 
 #1- Boston House dataset download
-batch_size=50
+#batch_size=50
 X, y = load_boston(return_X_y=True)
+#print(np.mean(X,axis=0))
 X = preprocessing.normalize(X)
-train_loader = DataLoader(dataset=X, batch_size=batch_size, shuffle=True)
+#print(np.mean(X_normalized,axis=0))
 
-X= next(iter(train_loader))
+
+#train_loader = DataLoader(dataset=X, batch_size=batch_size, shuffle=True)
+
+#X= next(iter(train_loader))
 X = torch.tensor(X, requires_grad=True,dtype=torch.float)
 y = torch.tensor(y, requires_grad=True,dtype=torch.float)
 
+y = y.view(len(y),1)
 #2- Fully connected network
 # 13 feature > 512> 512 > 1
 # loss function MSE (y-activation)^2
@@ -32,12 +37,12 @@ y = torch.tensor(y, requires_grad=True,dtype=torch.float)
 print("data size {}".format(X.shape))
 
 class Neural_Net(nn.Module):
-    def __init__(self,input_size,hidden_size,output_size):
+    def __init__(self,input_size,hidden_size1,hidden_size2,output_size):
         super(Neural_Net,self).__init__()
-        self.Linear1 = nn.Linear(input_size,hidden_size)
-        self.Linear2 = nn.Linear(hidden_size,hidden_size)
-        self.Linear3 = nn.Linear(hidden_size,hidden_size)
-        self.Linear4 = nn.Linear(hidden_size,output_size)
+        self.Linear1 = nn.Linear(input_size,hidden_size1)
+        self.Linear2 = nn.Linear(hidden_size1,hidden_size2)
+        self.Linear3 = nn.Linear(hidden_size2,output_size)
+        
         self.activation = nn.ReLU()
 
     def forward(self,x):
@@ -46,44 +51,41 @@ class Neural_Net(nn.Module):
         output = self.Linear2(output)
         output = self.activation(output)
         output = self.Linear3(output)
-        output = self.activation(output)
-        output = self.Linear4(output)
+        
+        
         # Consider adding activation function
         return output
 
-model = Neural_Net(2,512,1)
-
-
-# Training
-criterion = nn.L1Loss()
-epochs = 200
-optimizer = torch.optim.SGD(model.parameters(),lr=0.03,momentum=0.8)
-model = Neural_Net(13,32,1)
-
+model = Neural_Net(13,128,64,1)
 
 # Training
 criterion = nn.MSELoss()
 
-epochs = 100
+epochs = 1000
 
-optimizer = torch.optim.SGD(model.parameters(),lr=0.03,momentum=0.9)
+optimizer = torch.optim.SGD(model.parameters(),lr=0.003,momentum=0.9)
 plot_loss=[]
 
 for epoch in range(epochs):
     optimizer.zero_grad()
     output = model(X)
-    loss = criterion(y,output)
+    loss = criterion(output,y)
     loss.backward()
     optimizer.step()
     plot_loss.append(loss)
 
-plt.plot(range(epochs),plot_loss)
-plt.show()
+#plt.plot(range(epochs),plot_loss)
+#plt.show()
+
+
 
 plot_array = [i.detach().numpy() for i in plot_loss]
 plt.plot(range(epochs),plot_array )
 plt.show()
+'''
 
 #mean_error=np.mean(np.abs(output.detach().numpy()-y.detach().numpy()))
 #np.mean(y)-output
 #y = [i.detach().numpy() for i in y]
+
+'''
