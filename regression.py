8eadc00d1f8d03366df13020a4031057b2126f7b
cihankatar <katarcihan@gmail.com>
2023-01-03 23:35:41 +0300

    does work

--- regression.py
+++ regression.py
@@ -4,7 +4,7 @@ from sklearn.datasets import load_boston
 from sklearn import preprocessing
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.model_selection import train_test_split
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data.dataloader import DataLoader
 
@@ -15,21 +15,27 @@ from torch.utils.data.dataloader import DataLoader
 #4- Print loss
 
 #1- Boston House dataset download
-#batch_size=50
 X, y = load_boston(return_X_y=True)
 #print(np.mean(X,axis=0))
-X = preprocessing.normalize(X)
+
+#data preprocessing           ---------------
+from sklearn.preprocessing import StandardScaler
+scaler=StandardScaler()
+scaler.fit(X)
+X= scaler.transform(X)
+
+#X = preprocessing.normalize(X)
 #print(np.mean(X_normalized,axis=0))
-
-
 #train_loader = DataLoader(dataset=X, batch_size=batch_size, shuffle=True)
-
 #X= next(iter(train_loader))
+
 X = torch.tensor(X, requires_grad=True,dtype=torch.float)
 y = torch.tensor(y, requires_grad=True,dtype=torch.float)
 
 y = y.view(len(y),1)
-#2- Fully connected network
+
+
+#2 Fully connected network   -------------------
 # 13 feature > 512> 512 > 1
 # loss function MSE (y-activation)^2
 # activation function ReLU
@@ -57,12 +63,9 @@ class Neural_Net(nn.Module):
         return output
 
 model = Neural_Net(13,128,64,1)
-
 # Training
-criterion = nn.MSELoss()
-
+criterion = nn.L1Loss()
 epochs = 1000
-
 optimizer = torch.optim.SGD(model.parameters(),lr=0.003,momentum=0.9)
 plot_loss=[]
 
@@ -74,18 +77,6 @@ for epoch in range(epochs):
     optimizer.step()
     plot_loss.append(loss)
 
-#plt.plot(range(epochs),plot_loss)
-#plt.show()
-
-
-
 plot_array = [i.detach().numpy() for i in plot_loss]
 plt.plot(range(epochs),plot_array )
 plt.show()
-'''
-
-#mean_error=np.mean(np.abs(output.detach().numpy()-y.detach().numpy()))
-#np.mean(y)-output
-#y = [i.detach().numpy() for i in y]
-
-'''
