9d30fd089e8239b949338c41bd1b281aeea3cbf4
Omer Eryilmaz <omerb.eryilmaz@gmail.com>
2023-01-08 15:17:54 +0000

    so far so good

--- Modular_Learning/reference_code.py
+++ Modular_Learning/reference_code.py
@@ -14,10 +14,9 @@ def one_hot_encode(target, n_classes):
     if target.ndim > 1:
         target = np.squeeze(target)
     target_onehot = np.zeros((target.shape[0], n_classes))
-    # target_onehot[range(target.size(0)), target] = 1
-    # target_onehot[range(target.size(0)).long(), target.long()] = 1
     target_onehot[range(target.shape[0]), target.astype(int)] = 1
     return target_onehot
+
 target = one_hot_encode(target,num_classes)
 
 data = torch.tensor(data,requires_grad=True,dtype=torch.float) # 150,4
@@ -27,7 +26,7 @@ class MyModule(nn.Module):
     def __init__(self, sizes):
         super().__init__()
         self.activation = nn.ReLU()
-        self.softmax = nn.Softmax() 
+        self.tanh = nn.Tanh() 
         self.layers = nn.ModuleList([nn.Linear(in_f, out_f) for in_f, out_f in zip(sizes, sizes[1:])])
         self.outputs = []
 
@@ -38,12 +37,17 @@ class MyModule(nn.Module):
             if layer != self.layers[-1]:
                 x = self.activation(x)
             else:
-                x = self.softmax(x)
+                x = self.tanh(x)
             self.outputs.append(x)
 
-        return x
+        return x / (torch.sqrt(torch.sum(torch.power(torch.abs(x), 2), axis=1, keepdims=True)))
 
-model = MyModule([4, 16, 32, 3])# 1,16- 16-32 , 32- 2 
+model = MyModule([4, 16, 32, 2])# 1,16- 16-32 , 32- 2 
+xx = 
+def srs_loss(xx,ideal):
+    return -torch.mean(torch.exp(xx[target!=1]))
+
+
 criterion = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(),lr=0.001,momentum=0.9)
 
@@ -69,4 +73,10 @@ print(f'loss: {loss}')
 
 
 
-
+"""
+"""
+def tanh_norm(inputs):
+    z = tanh(inputs)
+    znorm = z / (np.sqrt(np.sum(np.power(np.abs(z), 2), axis=1, keepdims=True)))
+    return znorm
+"""
